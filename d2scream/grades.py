2b8392a37deccdb160b116f453746b49b77eb515
Nicholas Paun <nicholaspaun@gmail.com>
2019-02-06 10:35:06 -0500

    Make the Grade class docs less misleading by putting the fields in a constructor, also deal with strange uses of the weight field

--- d2scream/grades.py
+++ d2scream/grades.py
@@ -8,49 +8,50 @@ import re
 import json
 
 class Grade(object):
-    """
-    title:str option 
-    The title as it appears in the gradebook.
-    """
-    title = None
+    def __init__(self):
+        """
+        title:str option 
+        The title as it appears in the gradebook.
+        """
+        self.title = None
 
-    """
-    category:bool
-    True if this is a category of grade items, rather than a particular assessment
-    """
-    category = False
+        """
+        category:bool
+        True if this is a category of grade items, rather than a particular assessment
+        """
+        self.category = False
 
-    """
-    points:float option*float option 
-    Points achieved, out of the total possible points.
-    An entry may be None if it was not recorded in the gradebook.
-    """
-    points = (None,None)
+        """
+        points:float option*float option 
+        Points achieved, out of the total possible points.
+        An entry may be None if it was not recorded in the gradebook.
+        """
+        self.points = (None,None)
 
-    """
-    notes:str list option 
-    System notes, if any, regarding the points (e.g. "Dropped!")
-    2scream does not interpret this field, your application should make use of then notes which pertain to it.
-    """
-    notes = []
+        """
+        notes:str list option 
+        System notes, if any, regarding the points (e.g. "Dropped!")
+        2scream does not interpret this field, your application should make use of then notes which pertain to it.
+        """
+        self.notes = []
 
-    """
-    weight: float * float
-    Weight (i.e. portion of the final grade) out of the total possible weight.
-    """
-    weight = (0.0,0.0)
+        """
+        weight: float * float
+        Weight (i.e. portion of the final grade) out of the total possible weight.
+        """
+        self.weight = (0.0,0.0)
 
-    """
-    grade: any
-    The grade as recorded in the gradebook. Generally a float, but may be None if not entered, or a string if the instructor entered a non-standard value
-    """
-    grade = None
+        """
+        grade: any
+        The grade as recorded in the gradebook. Generally a float, but may be None if not entered, or a string if the instructor entered a non-standard value
+        """
+        self.grade = None
 
-    """
-    feedback: str option
-    Feedback comments as a string.
-    """
-    feedback = None
+        """
+        feedback: str option
+        Feedback comments as a string.
+        """
+        self.feedback = None
 
     def __repr__(self):
         return self.__dict__.__repr__()
@@ -109,7 +110,7 @@ def dump_grades(f):
 
 
         if s(cols[2]):
-            struct.weight = tuple(float(n) for n in s(cols[2]).split(' / '))
+            struct.weight = tuple(float(n) if n != '-' else None for n in s(cols[2]).split(' / '))
         
         if s(cols[3]):
             grade = s(cols[3]).rstrip('%').strip()
@@ -123,7 +124,7 @@ def dump_grades(f):
             struct.grade = grade
 
 
-        if s(cols[4]):
+        if len(cols) >= 5 and s(cols[4]):
             inner_div = cols[4].find('div', {'class': 'd2l-grades-individualcommentlabelcontainer'}).find('div')
             if s(inner_div):
                 feedback = strip_all(inner_div.findAll('div', recursive = False))
